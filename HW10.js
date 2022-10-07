cd508d7c5bcb76fe2e5de56013b66461be9f6e4d
edward-wong27 <ewon0007@student.monash.edu>
2022-10-07 23:54:48 +1100

    thing

---
+++ HW10.js
@@ -0,0 +1,5 @@
+Var = vg_1 = "HW10.json";
+vegaEmbed("#map1", vg_1).then(function(result) {
+    // Access the vega veiw instance(https://vega.github.io/vega/docs/api/view/) as result.veiw 
+
+}).catch(console.error);
