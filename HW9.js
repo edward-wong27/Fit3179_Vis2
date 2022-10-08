abd32cf50257d5120718d77a95c91ffaaf991ac0
edward-wong27 <ewon0007@student.monash.edu>
2022-10-09 02:47:12 +1100

    d

--- HW9.js
+++ HW9.js
@@ -3,3 +3,9 @@ vegaEmbed("#map", vg_1).then(function(result) {
     // Access the vega veiw instance(https://vega.github.io/vega/docs/api/view/) as result.veiw 
 
 }).catch(console.error);
+
+Var = vg_2 = "HW10.json";
+vegaEmbed("#Bar", vg_2).then(function(result) {
+    // Access the vega veiw instance(https://vega.github.io/vega/docs/api/view/) as result.veiw 
+
+}).catch(console.error);
