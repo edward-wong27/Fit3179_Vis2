f2560ab7039432dd9c89a2d2e20273f82ce74bfb
edward-wong27 <ewon0007@student.monash.edu>
2022-10-08 14:21:53 +1100

    update

--- HW10.js
+++ HW10.js
@@ -1,5 +1,5 @@
-Var = vg_1 = "HW10.json";
-vegaEmbed("#map1", vg_1).then(function(result) {
+Var = vg_2 = "HW10.json";
+vegaEmbed("#map1", vg_2).then(function(result) {
     // Access the vega veiw instance(https://vega.github.io/vega/docs/api/view/) as result.veiw 
 
 }).catch(console.error);
