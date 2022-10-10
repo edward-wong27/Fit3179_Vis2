7cdbf11ca65278e202e736d348fc888fc88fe29e
edward-wong27 <ewon0007@student.monash.edu>
2022-10-10 21:24:59 +1100

    g

--- VegaLite_Vis.js
+++ VegaLite_Vis.js
@@ -9,3 +9,9 @@ vegaEmbed("#Bar", vg_2).then(function(result) {
     // Access the vega veiw instance(https://vega.github.io/vega/docs/api/view/) as result.veiw 
 
 }).catch(console.error);
+
+Var = vg_3 = "LineChart.json";
+vegaEmbed("#Line", vg_3).then(function(result) {
+    // Access the vega veiw instance(https://vega.github.io/vega/docs/api/view/) as result.veiw 
+
+}).catch(console.error);
